6f71c824b903ed4f1b90710eee15df8e21c5b63c
madhawaawishka <madhawaawishka@gmail.com>
2025-11-17 13:35:19 +0530

    add Mongoose module integration to AppModule for MongoDB connection

--- apps/logs-service/src/app/app.module.ts
+++ apps/logs-service/src/app/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LogsModule } from './logs/logs.module';
 
 @Module({
-  imports: [LogsModule],
+  imports: [
+    LogsModule,
+    MongooseModule.forRoot(
+      process.env.MONGO_URI || 
+      'mongodb+srv://backend:qOcyo4PRHs9qGuy0@cluster0.19todyv.mongodb.net/travel-app-logs?retryWrites=true&w=majority'
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
